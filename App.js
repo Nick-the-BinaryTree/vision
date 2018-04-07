96e9863584f209b50d4b40385aad1551965fd38e
nick11h <nick11hd@gmail.com>
2018-04-07 08:45:47 -0400

    wimpy buttons

--- App.js
+++ App.js
@@ -1,23 +1,79 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {stage: "read"}
+    // start, scanning, read, return
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return (this.state.stage === "start" ?
+        <View style={styles.container}>
+          <Text style={styles.title}>Good Name</Text>
+        </View>
+    : this.state.stage === "scanning" ?
+      // camera business
+      <Text> hey </Text>
+    : this.state.stage === "read" ?
+        <View style={styles.container}>
+          <View style={[styles.button, styles.topButton]}>
+              <Text style={styles.buttonText}> hey </Text>
+          </View>
+            <View style={[styles.button, styles.bottomButton]}>
+                <Text style={styles.buttonText}> hey </Text>
+            </View>
+        </View>
+        : <Text> hey </Text>
+      );
   }
 }
+
+/*
+<TouchableNativeFeedback style={styles.buttonWrapper} onLongPress={() => (console.log())}>
+  <View style={styles.button, styles.topButton}>
+      <Text style={styles.buttonText}> hey </Text>
+  </View>
+</TouchableNativeFeedback>
+<View>
+  <TouchableNativeFeedback style={styles.buttonWrapper} onLongPress={() => (console.log())}>
+    <View style={styles.button, styles.bottomButton}>
+        <Text style={styles.buttonText}> hey </Text>
+    </View>
+  </TouchableNativeFeedback>
+*/
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#4FC3F7',
+    alignItems: 'stretch',
+    justifyContent: 'space-around'
   },
+  buttonWrapper: {
+  },
+  button: {
+    flex: .1,
+    flexDirection: 'row',
+    padding: 100,
+    justifyContent: 'center'
+  },
+  topButton: {
+    backgroundColor: '#4DD0E1'
+  },
+  bottomButton: {
+    backgroundColor: '#E57373'
+  },
+  buttonText: {
+    fontSize: 30
+  },
+  title: {
+    fontSize: 45,
+    fontWeight: 'bold',
+    color: 'white',
+    textShadowColor: 'rgba(0, 0, 0, 0.60)',
+    textShadowOffset: {width: 0, height: 1},
+    textShadowRadius: 10
+  }
 });
