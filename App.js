6cf71ad1111333e7ff5d3ee2c132526bf3299a5e
nick11h <nick11hd@gmail.com>
2018-04-07 11:01:07 -0400

    camera doesn't like touchable

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native';
+import { StyleSheet, Text, TouchableNativeFeedback, Vibration, View } from 'react-native';
+import { Camera, Permissions } from 'expo';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {stage: "read"}
+    this.state = {stage: "scanning", hasCameraPermission: null,
+      photoId: 0}
     // start, scanning, read, return
+    this.takePicture = this.takePicture.bind(this);
+  };
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  };
+  async takePicture() {
+    console.log('hey')
+    console.log(this.camera)
+    if (this.camera) {
+      this.camera.takePictureAsync().then(data => {
+        FileSystem.moveAsync({
+          from: data.uri,
+          to: `${FileSystem.documentDirectory}photos/Photo_${this.state.photoId}.jpg`,
+        }).then(() => {
+          this.setState({
+            photoId: this.state.photoId + 1,
+          });
+          Vibration.vibrate();
+        });
+      });
+    }
   }
   render() {
+    const { hasCameraPermission } = this.state;
     return (this.state.stage === "start" ?
         <View style={styles.container}>
           <Text style={styles.title}>Good Name</Text>
         </View>
     : this.state.stage === "scanning" ?
       // camera business
-      <Text> hey </Text>
+      <TouchableNativeFeedback onPress={this.takePicture}>
+        <View style={styles.container}>
+          <Camera style={{ flex: .8 }} type={this.state.type} ref={ref => {
+            this.camera = ref; }}>
+            <TouchableNativeFeedback
+              style={{ flex: 0.3, alignSelf: 'flex-end' }}
+              onPress={this.takePicture.bind(this)}>
+            <Text style={styles.buttonText}> SNAP </Text>
+          </TouchableNativeFeedback>
+          </Camera>
+        </View>
+      </TouchableNativeFeedback>
     : this.state.stage === "read" ?
         <View style={styles.container}>
           <TouchableNativeFeedback style={styles.buttonWrapper} onLongPress={() => (console.log())}>
